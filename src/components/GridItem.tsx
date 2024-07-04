66a64582324573b4e5a0afbd6191791e46c23608
tara-matthew <taramatthew.93@gmail.com>
2024-07-04 13:07:39 +0100

    Add in heart icon

--- src/components/GridItem.tsx
+++ src/components/GridItem.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import {Text, StyleSheet, View, Image} from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { Feather } from '@expo/vector-icons';
+import { Entypo } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
 
 const GridItem = ({item, style}) => {
     const width = 300;
@@ -7,6 +11,7 @@ const GridItem = ({item, style}) => {
 
     return (
         <View style={[style, styles.container]}>
+            <View style={styles.imageContainer}>
             <Image
                 style={{
                     width: width,
@@ -16,7 +21,16 @@ const GridItem = ({item, style}) => {
                     uri: item.images[0]?.url,
                 }}
             />
-            <Text style={{height: 80, fontSize: 18, padding: 8}}>{item.title}</Text>
+                <MaterialIcons
+                    name="favorite-outline"
+                    size={32}
+                    color="white"
+                    style={styles.icon}
+                />
+                {/*<Feather name="heart" size={32} color="black" style={styles.icon} />*/}
+
+            <Text style={{height: 45, fontSize: 18, padding: 8}}>{item.title}</Text>
+        </View>
         </View>
     )
 }
@@ -32,6 +46,20 @@ const styles = StyleSheet.create({
     },
     text: {
        textAlign: "center"
+    },
+    imageContainer: {
+        position: "relative"
+    },
+    icon: {
+        position: 'absolute',
+        top: 5,
+        right: 5,
+        textShadowColor: 'rgba(0, 0, 0, 0.75)',
+        textShadowOffset: {width: -1, height: 1},
+        textShadowRadius: 3
+        // opacity: 5
+        // backgroundColor: 'rgba(0, 0, 0, 0.5)'
+        // backgroundColor: 'red',
     }
 })
 
