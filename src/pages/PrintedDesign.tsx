45644efb8f4a889e75af504742585a15a0355128
tara-matthew <taramatthew.93@gmail.com>
2024-08-15 20:39:00 +0100

    Continue styling Printed Design screen

--- src/pages/PrintedDesign.tsx
+++ src/pages/PrintedDesign.tsx
@@ -1,13 +1,44 @@
-import React from "react";
-import {Text, View} from "react-native";
+import React, {useState} from "react";
+import {Text, View, Image, ScrollView, StyleSheet} from "react-native";
 
-const PrintedDesign = (print) => {
-    console.log(print.route.params.print.title)
+const PrintedDesign = (data) => {
+    console.log(data.route.params.print)
+    const print = data.route.params.print;
+    const imageUrl = print.images[0].url;
+    Image.getSize(imageUrl, (width, height) => {
+        console.log(width/height)
+        setAspectRatio(width/height)
+    })
+    const [aspectRatio, setAspectRatio] = useState(0);
+
     return (
-        <View>
-        <Text>{print.route.params.print.title}</Text>
+        <View className={"px-2"}>
+            <ScrollView contentContainerStyle={styles.container}>
+            <Image style={{
+                width: '100%',
+                height: undefined,
+                aspectRatio: aspectRatio,
+            }}
+                   source={{
+                uri: print.images[0]?.url,
+            }} />
+            <Text className={"text-center text-xl"}>{print.title}</Text>
+
+            </ScrollView>
         </View>
     )
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        paddingVertical: 20,
+        paddingHorizontal: 20
+    },
+    text: {
+        fontSize: 18,
+        marginBottom: 10
+    }
+});
 
 export default PrintedDesign;
