846152e37ce4ac9d6d80e1f6bbeae8c5c8b608a2
tara-matthew <taramatthew.93@gmail.com>
2024-08-21 11:18:53 +0100

    Redesign printed design screen

--- src/pages/PrintedDesign.tsx
+++ src/pages/PrintedDesign.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import {Text, View, Image, ScrollView, StyleSheet} from "react-native";
+import {Text, View, Image, ScrollView, StyleSheet, TouchableOpacity} from "react-native";
 
 const PrintedDesign = (data) => {
     const print = data.route.params.print;
@@ -22,13 +22,54 @@ const PrintedDesign = (data) => {
                    source={{
                 uri: print.images[0]?.url,
             }} />
-            <Text className={"text-center text-xl"}>{print.title}</Text>
+            <Text className={"text-center text-2xl mt-5 font-bold"}>{print.title}</Text>
                 <View className={'my-5'}>
-                    <Text className={'mb-4 font-bold'}>{print.description}</Text>
-                    <Text className={'mb-4'}>Material: {print.filament_material.name}</Text>
-                    <Text className={'mb-4'}>Filament Brand: {print.filament_brand.name}</Text>
-                    <Text className={'mb-4'}>Colour: {print.filament_colour.name}</Text>
+                    <Text className={'mb-4'}>{print.description}</Text>
+                    {/*<Text className={'mb-4'}>Printed on Ender-3</Text>*/}
+                    {/*<Text className={'mb-4'}>Material: {print.filament_material.name}</Text>*/}
+                    {/*<Text className={'mb-4'}>Brand: {print.filament_brand.name}</Text>*/}
+                    {/*<Text className={'mb-4'}>Colour: {print.filament_colour.name}</Text>*/}
+                    {/*<Text className={'mb-4'}>Infill: 20%</Text>*/}
                 </View>
+                <View className={"flex flex-row flex-wrap gap-y-4 gap-x-3 mb-9"}>
+                    <TouchableOpacity
+                        className={"bg-white p-3.5 rounded-2xl flex-grow"}
+                    >
+                        <Text className={"text-center text-black"}>Ender-3</Text>
+
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        className={"bg-white p-3.5 rounded-2xl flex-grow"}
+                    >
+                        <Text className={"text-center text-black"}>{print.filament_material.name}</Text>
+
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        className={"bg-white p-3.5 rounded-2xl flex-grow"}
+                    >
+                        <Text className={"text-center text-black"}>{print.filament_brand.name}</Text>
+
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        className={"bg-white p-3.5 rounded-2xl flex-grow"}
+                    >
+                        <Text className={"text-center text-black"}>{print.filament_colour.name}</Text>
+
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        className={"bg-white p-3.5 rounded-2xl flex-grow"}
+                    >
+                        <Text className={"text-center text-black"}>20% infill</Text>
+
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        className={"bg-white p-3.5 rounded-2xl flex-grow"}
+                    >
+                        <Text className={"text-center text-black"}>Tree supports</Text>
+
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.divider}></View>
 
             </ScrollView>
         </View>
@@ -47,6 +88,11 @@ const styles = StyleSheet.create({
     text: {
         fontSize: 18,
         marginBottom: 10
+    },
+    divider: {
+        width: '100%',
+        height: 1,
+        backgroundColor: '#DFE4EA'
     }
 });
 
