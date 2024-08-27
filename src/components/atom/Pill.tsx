e94e2b87041e36a6b89aab9522aa4866d266cb5f
tara-matthew <taramatthew.93@gmail.com>
2024-08-27 12:24:14 +0100

    Add type

--- src/components/atom/Pill.tsx
+++ src/components/atom/Pill.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import {Text, TouchableOpacity} from "react-native";
 
-const Pill = ({title}) => {
+type PillProps = {
+    title: string
+}
+
+const Pill = (props: PillProps) => {
     return (
         <TouchableOpacity
             className={"bg-white p-3.5 rounded-2xl"}
         >
-            <Text className={"text-center text-black"}>{title}</Text>
+            <Text className={"text-center text-black"}>{props.title}</Text>
 
         </TouchableOpacity>
     )
