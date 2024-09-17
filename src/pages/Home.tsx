7af8f5f48ac8cd57c64d1e3305918c52ad732c01
tara-matthew <taramatthew.93@gmail.com>
2024-09-17 14:35:37 +0100

    Refactor api calls on Home screen

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,42 +1,55 @@
-import React, {memo, useMemo} from 'react';
+import React, {memo, useEffect, useMemo, useState} from 'react';
 import {ScrollView, StyleSheet, Text, View} from "react-native";
 import ListGroup from "../components/template/ListGroup";
 import useFetch from "../hooks/useFetch";
+import axios from "axios";
 
 const Home = () => {
+    const baseURL = "https://wkz4a6zhju.sharedwithexpose.com/api"
+
+    const [loadingLatest, setLoadingLatest] = useState(true);
+    const [latestPrints, setLatestPrints] = useState([])
+    const [popularPrints, setPopularPrints] = useState([])
+    const [randomPrints, setRandomPrints] = useState([])
     const options = useMemo(() => ({
         method: 'GET'
     }), []);
 
-    const {
-        data: latestPrints,
-        loading: loadingLatest,
-        error: errorLatest,
-    } = useFetch('prints/latest', options);
+    useEffect(() => {
+        getHomeData()
+    }, []);
 
+    const getHomeData = () => {
+        let endpoints = [
+            `${baseURL}/prints/latest`,
+            `${baseURL}/my/prints`,
+            `${baseURL}/prints/random`,
+        ];
 
-    const {
-        data: popularPrints,
-        loading: loadingPopular,
-        error: errorPopular
-    } = useFetch('my/prints', options); // todo route alias
-
-    const {
-        data: randomPrints,
-        loading: loadingRandom,
-        error: errorRandom
-    } = useFetch('prints/random', options);
-
-    if (loadingLatest || loadingPopular || loadingRandom) {
-        return <Text>Loading...</Text>;
+        Promise.all(endpoints.map((endpoint) => axios.get(endpoint))).then(
+            (
+                [
+                    {data: latestPrints},
+                    {data: popularPrints},
+                    {data: randomPrints}
+                ]
+            )=> {
+            setLatestPrints(latestPrints.data)
+            setPopularPrints(popularPrints.data)
+            setRandomPrints(randomPrints.data)
+        });
     }
+
+    // if (loadingLatest) {
+    //     return <Text>Loading...</Text>;
+    // }
 
     return (
         <View className={"relative"}>
             <ScrollView contentContainerStyle={styles.container}>
                <ListGroup data={{heading: "Recently Uploaded", data: latestPrints}}></ListGroup>
                <ListGroup data={{heading: "Most Popular", data: popularPrints}}></ListGroup>
-               <ListGroup data={{heading: "Last Viewed", data: latestPrints}}></ListGroup>
+               <ListGroup data={{heading: "Last Viewed", data: randomPrints}}></ListGroup>
             </ScrollView>
         </View>
     );
