be09005474c1af4d93aca3cb2f0f89702c21a51b
tara-matthew <taramatthew.93@gmail.com>
2024-09-18 11:55:30 +0100

    Lint App.js

--- App.js
+++ App.js
@@ -1,45 +1,39 @@
-import React, {useContext, useEffect, useState} from 'react';
-import {View, FlatList, StyleSheet, Text, FlatListComponent, Image, ScrollView} from 'react-native';
-import List from "./src/components/organism/List";
-import useFetch from "./src/hooks/useFetch";
-import { Ionicons } from '@expo/vector-icons';
+import React from 'react';
+import { StyleSheet } from 'react-native';
 import "./global.css"
-import ListGroup from "./src/components/template/ListGroup";
 import Home from "./src/pages/Home";
-import Form from "./src/components/organism/Form";
-import Welcome from "./src/components/template/Welcome";
 import Login from "./src/pages/Login";
-import {NavigationContainer} from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import PrintedDesign from "./src/pages/PrintedDesign";
 import Thingiverse from "./src/pages/Thingiverse";
-import {AuthProvider, useAuth} from "./src/contexts/AuthContext";
+import { AuthProvider, useAuth } from "./src/contexts/AuthContext";
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
 const App = () => {
-        return (
-            <AuthProvider>
-                <Layout></Layout>
-            </AuthProvider>
-        );
+    return (
+        <AuthProvider>
+            <Layout></Layout>
+        </AuthProvider>
+    );
 };
 
 export const Layout = () => {
-    const {authState} = useAuth();
+    const { authState } = useAuth();
 
     return (
-            <NavigationContainer>
-                <Stack.Navigator>
-                    { authState?.authenticated ? (
-                        <Tab.Screen name="Home" component={Home} />) : (
-                        <Tab.Screen name="Login" component={Login} /> )}
-                    <Tab.Screen name="PrintedDesign" component={PrintedDesign} />
-                    <Tab.Screen name="Thingiverse" component={Thingiverse} />
-                </Stack.Navigator>
-            </NavigationContainer>
+        <NavigationContainer>
+            <Stack.Navigator>
+                { authState?.authenticated ? (
+                    <Tab.Screen name="Home" component={Home} />) : (
+                    <Tab.Screen name="Login" component={Login} /> )}
+                <Tab.Screen name="PrintedDesign" component={PrintedDesign} />
+                <Tab.Screen name="Thingiverse" component={Thingiverse} />
+            </Stack.Navigator>
+        </NavigationContainer>
     );
 }
 
@@ -49,7 +43,7 @@ const styles = StyleSheet.create({
         paddingHorizontal: 20
     },
     outerContainer: {
-      position: 'relative'
+        position: 'relative'
     },
     text: {
         fontSize: 18,
