f2274932fcac7c2aa8d9f3095d8942f200aaaa32
tara-matthew <taramatthew.93@gmail.com>
2024-10-10 14:22:49 +0100

    Use object destructuring

--- src/components/atom/Pill.tsx
+++ src/components/atom/Pill.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import { Text, TouchableOpacity } from "react-native";
 import { IPillProps } from "../../contracts/Pill";
 
-const Pill = (props: IPillProps) => {
+const Pill = ({ title }: IPillProps) => {
     return (
         <TouchableOpacity
             className="bg-white p-3.5 rounded-2xl"
         >
-            <Text className="text-center text-black">{props.title}</Text>
+            <Text className="text-center text-black">{title}</Text>
 
         </TouchableOpacity>
     );
