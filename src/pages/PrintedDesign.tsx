fb1eb7379d238227ccea456d516159b8f7afa4c0
tara-matthew <taramatthew.93@gmail.com>
2024-11-09 23:07:32 +0000

    Continue favouriting functionality

--- src/pages/PrintedDesign.tsx
+++ src/pages/PrintedDesign.tsx
@@ -1,19 +1,17 @@
-import React, {useEffect, useMemo, useState} from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { ScrollView, StyleSheet, Text, View, Button } from "react-native";
-import { useNavigation } from "@react-navigation/native";
 import PillGroup from "../components/molecule/PillGroup";
 import InfoCard from "../components/molecule/InfoCard";
 import ImageList from "../components/molecule/ImageList";
 import { Size } from "../contracts/Image";
-import {useAuth} from "../contexts/AuthContext";
+import { useAuth } from "../contexts/AuthContext";
+import apiFetch from "../hooks/apiFetch";
 
 const PrintedDesign = ({ route }) => {
-    const { print } = route.params;
+    const [print, setPrint] = useState(route.params.print);
     const { getUser } = useAuth();
     const [user, setUser] = useState<Partial<IUser>>({});
     // const [belongsToUser, setBelongsToUser] = useState(false);
-
-
 
     const pills = [
         // { title: "Ender-3" },
@@ -34,29 +32,51 @@ const PrintedDesign = ({ route }) => {
         };
 
         void fetchUser();
-
     }, []);
 
     const belongsToUser = user?.id === print.user_id;
     const favouriteText = useMemo(() => {
-       return print.is_favourite ? "Unfavourite" : "Favourite";
-    }, [print, print.is_favourite])
-    
-    const toggleFavourite = () => {
-        if (print.is_favourite) {
-            // call store favourite endpoint
-        } else {
-            // call remove favourite endpoint
-        }
-    }
+        return print.is_favourite ? "Unfavourite" : "Favourite";
+    }, [print, print.is_favourite]);
 
+    const toggleFavourite = async () => {
+        if (!print.is_favourite) {
+            await addToFavourites();
+        } else {
+            await removeFromFavourites();
+        }
+        toggleFavouriteStatus();
+    };
+
+    const addToFavourites = async () => {
+        try {
+            await apiFetch(`/favourites/printed_design/${print.id}`, "POST");
+        } catch (e) {
+            console.error(e);
+        }
+    };
+
+    const removeFromFavourites = async () => {
+        try {
+            await apiFetch(`/favourites/printed_design/${print.id}`, "DELETE");
+        } catch (e) {
+            console.error(e);
+        }
+    };
+
+    const toggleFavouriteStatus = () => {
+        setPrint(prevPrint => ({
+            ...prevPrint,
+            is_favourite: !prevPrint.is_favourite, // Toggle the favourite status
+        }));
+    };
 
     return (
         <ScrollView style={ { width: "100%" } }>
             <View style={ styles.imageContainer }>
                 <ImageList images={ print.images } size={ Size.Large } />
             </View>
-            {!belongsToUser && <Button onPress={toggleFavourite} title={favouriteText}></Button>}
+            {!belongsToUser && <Button onPress={ toggleFavourite } title={ favouriteText }></Button>}
 
             <View style={ styles.container }>
                 <Text className="text-center text-2xl mt-5 font-bold">{print.title}</Text>
