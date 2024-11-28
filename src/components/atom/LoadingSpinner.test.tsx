9079f103408d732eb05802062df94e0d47ab1419
tara-matthew <taramatthew.93@gmail.com>
2024-11-28 12:47:41 +0000

    Update LoadingSpinner to use Nativewind

--- src/components/atom/LoadingSpinner.test.tsx
+++ src/components/atom/LoadingSpinner.test.tsx
@@ -2,7 +2,22 @@ import React from "react";
 import { render } from "@testing-library/react-native";
 import LoadingSpinner from "./LoadingSpinner";
 
-it("renders correctly", () => {
-    const { toJSON } = render(<LoadingSpinner />);
-    expect(toJSON()).toMatchSnapshot();
+jest.useFakeTimers();
+
+describe("LoadingSpinner", () => {
+    afterEach(() => {
+        jest.clearAllTimers();
+    });
+
+    it("renders correctly", () => {
+        const { toJSON } = render(<LoadingSpinner />);
+        expect(toJSON()).toMatchSnapshot();
+    });
+
+    it("renders an ActivityIndicator", async () => {
+        const { getByRole } = render(<LoadingSpinner />);
+
+        const spinner = getByRole("progressbar");
+        expect(spinner).toBeTruthy();
+    });
 });
--- src/components/atom/LoadingSpinner.tsx
+++ src/components/atom/LoadingSpinner.tsx
@@ -1,29 +1,15 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { View } from "react-native";
 import { ActivityIndicator } from "react-native-paper";
 
 const LoadingSpinner = () => {
     return (
         <View
-            style={ styles.spinner }
+            className="absolute top-0 left-0 right-0 bottom-0 justify-center bg-white z-10"
         >
             <ActivityIndicator size="large" color="#0000ff" animating={ true } />
         </View>
     );
 };
-
-const styles = StyleSheet.create({
-    spinner: {
-        position: "absolute",
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "white",
-        zIndex: 1,
-    },
-});
 
 export default LoadingSpinner;
