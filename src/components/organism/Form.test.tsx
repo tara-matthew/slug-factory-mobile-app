d45cc19f2a2ca1fce0c0da494cc491cfa51c6387
tara-matthew <taramatthew.93@gmail.com>
2024-12-29 16:10:08 +0000

    Update form test

--- src/components/organism/Form.test.tsx
+++ src/components/organism/Form.test.tsx
@@ -3,38 +3,53 @@ import Form from "./Form";
 import React from "react";
 
 it("renders correctly", () => {
+    const mockSendDataToParent = jest.fn();
     const inputs = [
         {
+            title: "First input",
             placeholder: "First placeholder",
+            isMultiline: false,
         },
         {
+            title: "Second input",
             placeholder: "Second placeholder",
+            isMultiline: false,
         },
         {
+            title: "Third input",
             placeholder: "Third placeholder",
+            isMultiline: false,
         },
     ];
     const buttonText: string = "Click me";
     const buttonTo: string = "To link";
-    const { toJSON } = render(<Form inputs={ inputs } buttonText={ buttonText } buttonTo={ buttonTo } />);
+    const { toJSON } = render(<Form inputs={ inputs } buttonText={ buttonText } buttonTo={ buttonTo } sendDataToParent={ mockSendDataToParent } />);
     expect(toJSON()).toMatchSnapshot();
 });
 
 it("displays the correct inputs", () => {
+    const mockSendDataToParent = jest.fn();
     const inputs = [
         {
+            title: "First input",
             placeholder: "First placeholder",
+            isMultiline: false,
         },
         {
+            title: "Second input",
             placeholder: "Second placeholder",
+            isMultiline: false,
         },
         {
+            title: "Third input",
             placeholder: "Third placeholder",
+            isMultiline: false,
         },
     ];
+
     const buttonText: string = "Click me";
     const buttonTo: string = "To link";
-    const { getByPlaceholderText } = render(<Form inputs={ inputs } buttonText={ buttonText } buttonTo={ buttonTo } />);
+    const { getByPlaceholderText } = render(<Form inputs={ inputs } buttonText={ buttonText } buttonTo={ buttonTo } sendDataToParent={ mockSendDataToParent } />);
     const placeholders = [getByPlaceholderText("First placeholder"), getByPlaceholderText("Second placeholder"), getByPlaceholderText("Third placeholder")];
     placeholders.forEach(placeholder => expect(placeholder).toBeTruthy());
 });
