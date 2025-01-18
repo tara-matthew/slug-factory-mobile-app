3db30dc6b37946bd3d22a5073c1554ba6867cf2b
tara-matthew <taramatthew.93@gmail.com>
2025-01-18 13:36:57 +0000

    Add tests

---
+++ src/components/organism/TouchableElementList.test.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { render } from "@testing-library/react-native";
+import TouchableElementList from "./TouchableElementList";
+
+it("renders correctly", () => {
+    const profileMock = jest.fn();
+
+    const items = [
+        {
+            to: profileMock,
+            title: "My profile",
+        },
+        {
+            to: profileMock,
+            title: "Edit profile",
+        },
+        {
+            to: profileMock,
+            title: "My prints",
+        },
+    ];
+    const { toJSON } = render(<TouchableElementList items={ items } />);
+    expect(toJSON()).toMatchSnapshot();
+});
