5f9ab83458fa94fac3cf7a006555e10e7da4050a
tara-matthew <taramatthew.93@gmail.com>
2025-02-24 21:58:57 +0000

    Update setup.js

--- src/jest/setup.js
+++ src/jest/setup.js
@@ -1,30 +1,34 @@
-// jest.mock("@react-navigation/native", () => {
-//     return {
-//         ...jest.requireActual("@react-navigation/native"),
-//         useNavigation: jest.fn(() => ({})),
-//     };
-// });
+jest.mock("@react-navigation/native", () => {
+    return {
+        ...jest.requireActual("@react-navigation/native"),
+        useNavigation: jest.fn(() => ({})),
+    };
+});
 
 jest.mock("expo-image", () => {
     const { Image } = require("react-native");
     return { Image };
 });
 
-// jest.mock("expo-image-picker", () => ({
-//     launchImageLibraryAsync: jest.fn().mockResolvedValue({
-//         cancelled: false,
-//         assets: [{ uri: "mock-image-uri" }],
-//     }),
-//     launchCameraAsync: jest.fn().mockResolvedValue({
-//         cancelled: false,
-//         assets: [{ uri: "mock-image-uri" }],
-//     }),
-//     MediaTypeOptions: {
-//         Images: "Images",
-//         Videos: "Videos",
-//         All: "All",
-//     },
-// }));
+jest.mock("@react-native-async-storage/async-storage", () =>
+    require("@react-native-async-storage/async-storage/jest/async-storage-mock"),
+);
+
+jest.mock("expo-image-picker", () => ({
+    launchImageLibraryAsync: jest.fn().mockResolvedValue({
+        cancelled: false,
+        assets: [{ uri: "mock-image-uri" }],
+    }),
+    launchCameraAsync: jest.fn().mockResolvedValue({
+        cancelled: false,
+        assets: [{ uri: "mock-image-uri" }],
+    }),
+    MediaTypeOptions: {
+        Images: "Images",
+        Videos: "Videos",
+        All: "All",
+    },
+}));
 
 // jest.mock('react-native-paper', () => {
 //     const realModule = jest.requireActual('react-native-paper');
@@ -34,18 +38,22 @@ jest.mock("expo-image", () => {
 //     };
 // });
 
-// jest.mock("expo-image-manipulator", () => ({
-//     manipulateAsync: jest.fn().mockResolvedValue({
-//         uri: "mock-manipulated-image-uri",
-//         width: 100,
-//         height: 100,
-//         base64: null,
-//     }),
-// }));
+jest.mock("expo-image-manipulator", () => ({
+    manipulateAsync: jest.fn().mockResolvedValue({
+        uri: "mock-manipulated-image-uri",
+        width: 100,
+        height: 100,
+        base64: null,
+    }),
+}));
+
+// jest.mock("../pages/MyProfile");
 
 // jest.mock('../../src/contexts/UserContext', () => ({
 //     useUser: jest.fn(),
 // }));
+
+// jest.mock('../../src/contexts/AuthContext')
 
 const mockUser = { id: 1, name: "Test User" };
 const mockSetUser = jest.fn();
