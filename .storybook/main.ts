caeb4511ba581c563fd24c458eba7d2c243dac0d
tara-matthew <taramatthew.93@gmail.com>
2025-06-24 22:00:21 +0100

    Install Storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,20 @@
+import type { StorybookConfig } from '@storybook/react-native-web-vite';
+
+const config: StorybookConfig = {
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [
+    "@chromatic-com/storybook",
+    "@storybook/addon-docs",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-a11y",
+    "@storybook/addon-vitest"
+  ],
+  "framework": {
+    "name": "@storybook/react-native-web-vite",
+    "options": {}
+  }
+};
+export default config;
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
@@ -5,36 +8,27 @@ import pluginReact from "eslint-plugin-react";
 import unusedImports from "eslint-plugin-unused-imports";
 import stylistic from "@stylistic/eslint-plugin";
 
-export default [
-    stylistic.configs.customize({
-        indent: 4,
-        quotes: "double",
-        semi: true,
-    }),
-    {
-        settings: {
-            react: {
-                version: "detect",
-            },
-        },
-        plugins: {
-            "unused-imports": unusedImports,
-            "@stylistic": stylistic,
-        },
-        rules: {
-            "no-unused-vars": "error",
-            "unused-imports/no-unused-imports": "error",
-            "no-undef": "error",
-            "@stylistic/object-curly-spacing": ["error", "always"],
-            "@stylistic/jsx-curly-spacing": ["error", "always"],
-            "@stylistic/brace-style": ["error", "1tbs"],
-
+export default [stylistic.configs.customize({
+    indent: 4,
+    quotes: "double",
+    semi: true,
+}), {
+    settings: {
+        react: {
+            version: "detect",
         },
     },
-    { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-    { languageOptions: { globals: globals.browser } },
-    pluginJs.configs.recommended,
-    ...tseslint.configs.recommended,
-    pluginReact.configs.flat.recommended,
+    plugins: {
+        "unused-imports": unusedImports,
+        "@stylistic": stylistic,
+    },
+    rules: {
+        "no-unused-vars": "error",
+        "unused-imports/no-unused-imports": "error",
+        "no-undef": "error",
+        "@stylistic/object-curly-spacing": ["error", "always"],
+        "@stylistic/jsx-curly-spacing": ["error", "always"],
+        "@stylistic/brace-style": ["error", "1tbs"],
 
-];
+    },
+}, { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] }, { languageOptions: { globals: globals.browser } }, pluginJs.configs.recommended, ...tseslint.configs.recommended, pluginReact.configs.flat.recommended, ...storybook.configs["flat/recommended"], ...storybook.configs["flat/recommended"], ...storybook.configs["flat/recommended"]];
